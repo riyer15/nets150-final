868679fea11b8091e932fed18bf1886bdb5f6430
tstat1996 <tstat@seas.upenn.edu>
2016-04-20 13:04:44 -0400

    created review class

--- src/Review.java
+++ src/Review.java
@@ -1,4 +1,38 @@
 
 public class Review {
+    String userID;
+    String movieID;
+    String text;
+    String rating;
+    String helpfulness;
+    
+    public Review(String userID, String movieID, String text, String rating,
+            String helpfulness){
+        this.userID = userID;
+        this.movieID = movieID;
+        this.text = text;
+        this.rating = rating;
+        this.helpfulness = helpfulness;
+    }
+    
+    public String getUserID(){
+        return userID;
+    }
+    
+    public String getMovieID(){
+        return movieID;
+    }
+    
+    public String getText(){
+        return text;
+    }
+    
+    public String getRating(){
+        return rating;
+    }
+    
+    public String getHelpfulness(){
+        return helpfulness;
+    }
 
 }
