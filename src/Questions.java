2f0af63383de5859549ecfd02c37917dd851d725
snchung <chungsa@seas.upenn.edu>
2016-04-21 19:56:49 -0400

    file to answer questions in

---
+++ src/Questions.java
@@ -0,0 +1,39 @@
+
+public class Questions {
+	
+	//Question 1: Sam : If user A and user B both review Movie C, does this 
+	//mean they will both review Movie D?
+	public void question1() {
+		
+	}
+	
+	//Question 2: Sam : Does the same reviewer review movies all nicely or 
+	//all harshly (the rating number)?
+	public void question2() {
+		
+	}
+	
+	//Question 3: Talia : Do reviewers use similar words in all of their reviews?
+	public void question3() {
+		
+	}
+	
+	//Question 4: Talia : Do all the reviewers reviewing movie A use similar words to describe them?
+	public void question4() {
+		
+	}
+	
+	//Question 5: Rani : Does the helpfulness of the review correspond to the 
+	//style (negativity/positivity) of which the review is written?
+	public void question5() {
+		
+	}
+	
+	//Question 6: Rani : Choose a movie: what are trends we notice about the 
+	//words used in describing the movie? Are these trends picked up from other 
+	//reviews of the same movie, or from all the movie reviews in general?
+	public void quesion6() {
+		
+	}
+
+}
