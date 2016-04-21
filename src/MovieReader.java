1a367a5abcbe7e7925f64385027bbc159b630506
riyer15 <rani.iyer15@gmail.com>
2016-04-21 16:56:51 -0400

    fixed the creation of objects in the MovieReader class

--- src/MovieReader.java
+++ src/MovieReader.java
@@ -6,14 +6,24 @@ import java.io.Reader;
 import java.util.*;
 
 public class MovieReader {
-    Set<Movie> movieSet;
-    Set<User> userSet;
-    Set<Review> reviewSet;
+	/*For later parts in the code, we need to access previously made movie/
+	 * user/review objects. We can't just add our "dummy" objects because 
+	 * then we lose all the data we're storing (specifically in their adjacency lists)
+	 *  Sets don't allow you to find a specific object without using an iterator, (look it up)
+	 * so I changed their representation to Lists. Though the List doesn't guarantee 
+	 * uniqueness, we check for pre-existence in our code when we 
+	 * create our objects, so the list implementation is basically unique anyway. 
+	 * - Rani 
+	 */
+    List<Movie> movieSet; 
+   
+    List<User> userSet;
+    List<Review> reviewSet;
     
     public MovieReader() throws IOException{
-        movieSet = new HashSet<Movie>();
-        userSet = new HashSet<User>();
-        reviewSet = new HashSet<Review>();
+        movieSet = new ArrayList<Movie>(); 
+        userSet = new ArrayList<User>();
+        reviewSet = new ArrayList<Review>();
         
         Reader r = new FileReader("movies.txt");
         BufferedReader br1 = new BufferedReader(r);
@@ -65,19 +75,46 @@ public class MovieReader {
             Review re = new Review(userID, productID, text, score, helpfulness);
             User u = new User(userID, mov, rev, profileName);
             Movie m = new Movie(productID, rev, user);
+            
+            /*
+             * For this part: keep in mind that once we add an object to
+             * another object's adjacency lists, any changes we made to the first 
+             * object *will* be updated in the other object's list, because we're passing
+             * by reference. For example, if I add a Movie A to a User B, and then add
+             * Review C to Movie A, Movie A in User B's list will have has Review C. 
+             * That's why fact that some of the objects are incomplete when we add them
+             * to the list isn't that big of a deal -- updates to one object are reflected
+             * in all their associated objects. Text me if you're still confused (or maybe
+             * look up the 120 Notes?) - Rani 
+             */
             if(movieSet.contains(m)){
-                if(userSet.contains(u)){
-                    m.addReview(re, u);
+            	m = movieSet.get(movieSet.indexOf(m));
+            	if(userSet.contains(u)){
+            		u = userSet.get(userSet.indexOf(u));
+            		m.addReview(re, u);
+            		u.addReview(re);
                 }
                 else{
                     userSet.add(u);
-                    //u.add
+                    u.addReview(re);
+                    m.addReview(re, u);                    
                 }
                 
             }
             else{
                 movieSet.add(m);
+                if(userSet.contains(u)){
+                	u = userSet.get(userSet.indexOf(u));
+                	u.addReview(re);
+                	m.addReview(re, u);
+                }
+                else{
+                userSet.add(u);
+                u.addReview(re);
+                m.addReview(re, u);
+                }
             }
+            
                // }
            // }
             x = br1.readLine();
